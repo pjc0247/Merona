405bd197035bbad080c5f5953b14a54a0a71721a
Anz_MSI <pjc0247@naver.com>
2013-02-08 03:33:31 +0900

    초코파이 api 이식 - Debug
    chocopie api ver 1.4.1

---
+++ Merona/lib/mebase/debug/log.rb
@@ -0,0 +1,26 @@
+﻿###################################################
+#             ChocoPie GameEngine                 #
+#                           Log                   #
+###################################################
+
+def defLogger(msg)
+    puts "[LOG] : " + msg
+end
+
+class Log
+    @@logfunc = method(:defLogger)
+
+    def initialize
+    end
+    def dispose
+    end
+
+    def self.logfunc(lf)
+        @@logfunc = lf
+    end
+
+    def self.output(msg)
+        @@logfunc.call msg
+    end
+end
+
