02f0553d714034708eadc5cc4bb0200834ad8a1d
Anz_MSI <pjc0247@naver.com>
2013-02-08 03:34:53 +0900

    초코파이 api 이식 - Version
    chocopie api ver 1.4.1

---
+++ Merona/lib/mebase/core/version.rb
@@ -0,0 +1,31 @@
+﻿class Version
+    attr_accessor :major, :minor, :fix
+
+    def initialize(major=1,minor=0,fix=0)
+        self.set(major,minor,fix)
+    end
+    def dispose
+    end
+
+    def set(major,minor,fix)
+        @major = major
+        @minof = minor
+        @fix = fix
+    end
+end
+
+def getMajorVersion
+    return 1
+end
+def getMinorVersion
+    return 0
+end
+def getFixVersion
+    return 0
+end
+
+def getVersion
+    ver = Version.new(
+        getMajorVersion,getMinorVersion,getFixVersion)
+    return ver
+end
