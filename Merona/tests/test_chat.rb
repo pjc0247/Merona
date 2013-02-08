33242a6f4e1111de524d6edca8c4cbb93e94ea8a
Anz_MSI <pjc0247@naver.com>
2013-02-09 01:48:35 +0900

    test_chat 추가

---
+++ Merona/tests/test_chat.rb
@@ -0,0 +1,23 @@
+require 'socket'
+
+load '../lib/mebase/protocol/chat_protocol.rb'
+load '../lib/mebase/packet/packet.rb'
+load '../lib/mebase/packet/chat_packet.rb'
+
+load 'test_util.rb'
+
+if ARGV.size == 2
+	addr = ARGV[0]
+	port = ARGV[1]
+else
+	addr = "localhost"
+	port = 9916
+end
+sock = TCPSocket.new addr, port
+
+packet = ChatPacket.new
+packet.id = 1301
+packet.sender = "Aqwer"
+packet.msg = "AEWF"
+
+send_obj(sock, packet)
