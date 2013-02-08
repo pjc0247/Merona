ef5d66e4bf1db502aa4a17479349c236d3e8f9ec
Anz_MSI <pjc0247@naver.com>
2013-02-09 01:17:37 +0900

    Connection 따로 파일로 분리

---
+++ Merona/lib/mebase/server/connection.rb
@@ -0,0 +1,36 @@
+require 'eventmachine'
+
+class Connection < EM::Connection
+	include EM::P::ObjectProtocol
+	
+	attr_reader :ip, :port
+	
+	def initialize(*args)
+		@port, @ip = Socket.unpack_sockaddr_in(get_peername)
+		@server = args[0]
+		@server.clients.push self
+		
+		connect
+	end
+	def unbind
+		@server.clients.delete self
+		disconnect
+	end
+	def receive_object(obj)
+		@server.handler.each do |handler|
+			handler.recv(obj)
+		end
+	end
+	
+	
+	def self.clients
+		@@clients
+	end
+	
+	def connect
+		Log.output("new connection from " + @ip)
+	end
+	def disconnect
+		Log.output("lost connection " + @ip)
+	end
+end
