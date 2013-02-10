19c9004161176d66ce78bd9195fa5fcd3df6aa7d
Anz_MSI <pjc0247@naver.com>
2013-02-10 10:26:21 +0900

    Pubsub 프로토타�

---
+++ Merona/lib/mebase/pubsub/pubsub.rb
@@ -0,0 +1,31 @@
+﻿class Pubsub
+	def initialize
+		@channel = {}
+	end
+	def dispose
+	end
+	
+	def subscribe(name)
+		if @channel[name] == nil
+			@channel[name] = Array.new
+		end
+		
+		@channel[name].push name
+	end
+	def unsubscribe(name)
+		@channel[name].delete name
+		
+		if @channel[name].size == 0
+			@channel[name] = nil
+		end
+	end
+	def publish(name, packet)
+		if @channel[name] == nil
+			@channel[name] = Array.new
+		end
+		
+		@channel[name].each do |client|
+			client.send packet
+		end
+	end
+end
