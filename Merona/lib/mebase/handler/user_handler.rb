7ac7d2427f9e87b3297abe442469b4381eeed23d
Anz_MSI <pjc0247@naver.com>
2013-02-12 14:09:48 +0900

    User handler prototype

---
+++ Merona/lib/mebase/handler/user_handler.rb
@@ -0,0 +1,10 @@
+class ChatHandler < Handler
+	def recv(server,connection,packet)
+		case packet.id
+			when Protocol::REGIST
+			when Protocol::UNREGIST
+			when Protocol::LOGIN
+			when Protocol::LOGOUT
+		end
+	end
+end
