135b00736e060b117f53812a377a72729c6b569d
Anz_MSI <pjc0247@naver.com>
2013-02-19 22:25:46 +0900

    WorkerState 추가

--- Merona/lib/mebase/process/worker_connection.rb
+++ Merona/lib/mebase/process/worker_connection.rb
@@ -1,8 +1,22 @@
 require 'eventmachine'
 
+class WorkerState
+	BUSY = 1
+	IDLE = 2
+end
+
 class WorkerConnection < Connection
+	attr_reader :state
+	
 	def initialize(*args)
+		@state = WorkerState::IDLE
 	end
 	def unbind
 	end
+	
+	def receive_object(obj)
+		@state = BUSY
+		super obj
+		@state = IDLE
+	end 
 end
