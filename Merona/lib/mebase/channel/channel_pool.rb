df5afb543495c93e0d8305603d6e1f5e347809b5
Anz_MSI <pjc0247@naver.com>
2013-02-19 20:57:10 +0900

    채널이름 토큰화 처리

--- Merona/lib/mebase/channel/channel_pool.rb
+++ Merona/lib/mebase/channel/channel_pool.rb
@@ -8,9 +8,27 @@ class ChannelPool
 	def create_channel
 	end
 	
+	def tokenize(name, &block)
+		token = name.split(".")
+		if token.size > 1
+			ch = ""
+			for i in 0..token.size-2
+				ch += token[i] + "."
+			end
+			ch = ch[0..ch.length-2]
+			
+			block.call ch
+		end
+	end
+	
 	def subscribe(name, client)
 		if @channel[name] == nil
 			@channel[name] = Array.new
+		end
+		
+		puts name
+		tokenize(name) do |t|
+			subscribe t, client
 		end
 		
 		@channel[name].push client
@@ -18,11 +36,19 @@ class ChannelPool
 	def unsubscribe(name, client)
 		@channel[name].delete client
 		
+		tokenize(name) do |t|
+			unsubscribe t, client
+		end
+		
 		if @channel[name].size == 0
 			@channel[name] = nil
 		end
 	end
 	def publish(name, packet)
+		tokenize(name) do |t|
+			publish t, packet
+		end
+		
 		if @channel[name] == nil
 		else
 			@channel[name].each do |client|
