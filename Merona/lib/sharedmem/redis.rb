369cce830ee5ecad4513d141f1880bedffac6220
Anz_MSI <pjc0247@naver.com>
2013-02-24 23:03:31 +0900

    Redis 생성자 오타 수정

--- Merona/lib/sharedmem/redis.rb
+++ Merona/lib/sharedmem/redis.rb
@@ -2,7 +2,7 @@ require 'redis'
 
 class RedisMemory < SharedMemory
 	def initialize(host="localhost", port=6679)
-		@redis = Redis.new(host, port)
+		@redis = Redis.new(:host => host, :port => port)
 	end
 	def dispose
 	end
