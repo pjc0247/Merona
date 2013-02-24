000454418781a5ad25349b84c3900e9f0579727e
Anz_MSI <pjc0247@naver.com>
2013-02-24 20:57:50 +0900

    ctrl + c 누를시 종료 처리 추가

--- Merona/lib/mebase/core/core.rb
+++ Merona/lib/mebase/core/core.rb
@@ -4,6 +4,11 @@ end
 
 def meInitialize
 	
+	Signal.trap("INT")  do
+		Log.output "quit by user"
+		EventMachine.stop
+	end
+  
 	t = Time.new 
 	logfile = "log/#{t.year}.#{t.month}.#{t.day}.log"
 	Log.logfile = File.new(logfile, "a")
