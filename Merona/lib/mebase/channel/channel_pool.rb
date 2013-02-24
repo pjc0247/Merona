ca7108210f3573b1554f0a30092999cb1522daa8
Anz_MSI <pjc0247@naver.com>
2013-02-24 18:18:33 +0900

    unsubscribe, publish에 대해 예외 처리 추가

--- Merona/lib/mebase/channel/channel_pool.rb
+++ Merona/lib/mebase/channel/channel_pool.rb
@@ -19,22 +19,24 @@ class ChannelPool
 	end
 	
 	def subscribe(name, client)
-		if @channel[name] == nil
-			@channel[name] = Array.new
-		end
-
 		tokenize(name) do |t|
 			subscribe t, client
+		end
+		
+		if @channel[name] == nil
+			@channel[name] = Array.new
 		end
 		
 		@channel[name].push client
 	end
 	def unsubscribe(name, client)
-		@channel[name].delete client
-		
 		tokenize(name) do |t|
 			unsubscribe t, client
 		end
+		
+		return if @channel[name] == nil
+		
+		@channel[name].delete client
 		
 		if @channel[name].size == 0
 			@channel[name] = nil
@@ -45,11 +47,10 @@ class ChannelPool
 			publish t, packet
 		end
 		
-		if @channel[name] == nil
-		else
-			@channel[name].each do |client|
-				client.send packet
-			end
+		return if @channel[name] == nil
+		
+		@channel[name].each do |client|
+			client.send packet
 		end
 	end
 end
