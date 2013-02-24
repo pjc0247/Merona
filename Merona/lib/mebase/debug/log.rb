ea91aa6fe188de488deac8266188f9a048e622af
Anz_MSI <pjc0247@naver.com>
2013-02-24 20:52:33 +0900

    log.rb 패치

--- Merona/lib/mebase/debug/log.rb
+++ Merona/lib/mebase/debug/log.rb
@@ -1,24 +1,41 @@
 ﻿def defLogger(msg)
-    puts "[LOG] : " + msg
+    puts msg
 end
 
 class Log
     @@logfunc = method(:defLogger)
-
+	@@logfile
+	
     def initialize
     end
     def dispose
     end
 
+	def self.logfile=(fp)
+		@@logfile = fp
+	end
+	
 	def self.logfunc
 		@@logfunc
 	end
     def self.logfunc=(lf)
         @@logfunc = lf
     end
-
+	
+	private
+	def self.time
+		t = Time.new
+		time = "[#{t.year}:#{t.month}:#{t.day}:#{t.hour}:#{t.min}:#{t.sec}]"
+	end
+	
+	def self.warn(msg)
+		@@logfunc.call time + " [WARN] : " + msg
+	end
+	def self.error(msg)
+		@@logfunc.call time + " [ERROR] : " + msg
+	end
     def self.output(msg)
-        @@logfunc.call msg
+        @@logfunc.call time + " [LOG] : " + msg
     end
 end
 
