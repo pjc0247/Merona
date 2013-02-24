0a0b43a638747705cd31d32af4be677c4a897206
Anz_MSI <pjc0247@naver.com>
2013-02-24 18:51:51 +0900

    LOGIN 핸들링 추가

--- Merona/lib/mebase/handler/user_handler.rb
+++ Merona/lib/mebase/handler/user_handler.rb
@@ -4,6 +4,20 @@ class UserHandler < Handler
 			when Protocol::REGIST
 			when Protocol::UNREGIST
 			when Protocol::LOGIN
+				rs = connection.db.select("account", "*", "id=#{packet["id"]} and pw=#{paacket["pw"]}")
+				row = rs.next
+				
+				reply = Packet.new
+				reply.id = LOGIN_RESULT
+				if row == nil
+					reply["result"] = nil
+				else
+					reply["result"] = true
+					connection.id = packet["id"]
+				end
+				
+				rs.close
+				connection.send reply
 			when Protocol::LOGOUT
 		end
 	end
