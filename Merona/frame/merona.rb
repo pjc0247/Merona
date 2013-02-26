a182a9660b17f985fb50b0a886b85314b48caad5
Anz_MSI <pjc0247@naver.com>
2013-02-27 08:46:07 +0900

    경로 오타 수정

--- Merona/frame/merona.rb
+++ Merona/frame/merona.rb
@@ -2,19 +2,19 @@
 
 require 'rubygems'
 
-meorna_path = Gem::bin_path('merona', 'merona')
-meorna_path.reverse!
-meorna_path = path.split("/", 3)[2]
-meorna_path.reverse!
-meorna_path += "/lib"
+$merona_path = Gem::bin_path('merona', 'merona')
+$merona_path.reverse!
+$merona_path = $merona_path.split("/", 3)[2]
+$merona_path.reverse!
+$merona_path += "/lib"
 
-$LOAD_PATH << meorna_path
+$LOAD_PATH << $merona_path
 $LOAD_PATH << File.dirname(File.expand_path(__FILE__))
 
 
 def import(_path)
 	if _path[_path.length-1] == '*'
-		path = meorna_path + _path
+		path = $merona_path + "/" + _path
 		Dir[path[0..path.length-1] + "*.rb"].each {|file| require file }
 		path = "./" + _path
 		Dir[path[0..path.length-1] + "*.rb"].each {|file| require file }
