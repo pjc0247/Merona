d7e640f310912ce17aca0db6a99c842f5f0608ec
Anz_MSI <pjc0247@naver.com>
2013-02-27 08:42:21 +0900

    LOAD_PATH 수정

--- Merona/frame/merona.rb
+++ Merona/frame/merona.rb
@@ -1,10 +1,20 @@
 # Merona entry
-$LOAD_PATH << "../lib"
-$LOAD_PATH << "."
+
+require 'rubygems'
+
+meorna_path = Gem::bin_path('merona', 'merona')
+meorna_path.reverse!
+meorna_path = path.split("/", 3)[2]
+meorna_path.reverse!
+meorna_path += "/lib"
+
+$LOAD_PATH << meorna_path
+$LOAD_PATH << File.dirname(File.expand_path(__FILE__))
+
 
 def import(_path)
 	if _path[_path.length-1] == '*'
-		path = "../lib/" + _path
+		path = meorna_path + _path
 		Dir[path[0..path.length-1] + "*.rb"].each {|file| require file }
 		path = "./" + _path
 		Dir[path[0..path.length-1] + "*.rb"].each {|file| require file }
