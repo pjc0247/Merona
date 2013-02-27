859fd0781856e30a4570c1de0569a0d0d3f2cac8
Anz_MSI <pjc0247@naver.com>
2013-02-27 18:25:47 +0900

    add comment

--- Merona/lib/channel/channel_pool.rb
+++ Merona/lib/channel/channel_pool.rb
@@ -5,6 +5,12 @@ class ChannelPool
 	def dispose
 	end
 	
+	# 채널 이름을 토큰화해서 최상위 레벨의 채널에 도달할 때 까지
+	# 코드 블록을 실행시킨다.
+	#
+	# ex ) "ch.map.party"
+	#       "ch.map"
+	#       "ch"
 	def tokenize(name, &block)
 		token = name.split(".")
 		if token.size > 1
@@ -18,6 +24,7 @@ class ChannelPool
 		end
 	end
 	
+	# 지정된 이름의 채널을 구독한다
 	def subscribe(name, client)
 		tokenize(name) do |t|
 			subscribe t, client
@@ -29,6 +36,8 @@ class ChannelPool
 		
 		@channel[name].subscribe client
 	end
+	
+	# 지정된 이름의 채널의 구독을 해지한다
 	def unsubscribe(name, client)
 		tokenize(name) do |t|
 			unsubscribe t, client
@@ -40,6 +49,8 @@ class ChannelPool
 			@channel[name].delete name
 		end
 	end
+	
+	# 지정된 이름의 채널에 패킷을 전송한다.
 	def publish(name, packet)
 		tokenize(name) do |t|
 			publish t, packet
