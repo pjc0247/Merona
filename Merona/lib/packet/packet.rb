86f97fd5e293dae6d9e34a2fe1afef7971234f12
Anz_MSI <pjc0247@naver.com>
2013-02-27 19:14:07 +0900

    add comment

--- Merona/lib/packet/packet.rb
+++ Merona/lib/packet/packet.rb
@@ -13,6 +13,7 @@ class Packet
 	def dispose
 	end
 	
+	# 패킷에 데이터를 넣는다
 	def push(name,data)
 		if name == nil
 			return
@@ -20,6 +21,8 @@ class Packet
 		
 		@data[name] = data
 	end
+	
+	# 패킷에서 데이터를 가져온다
 	def get(name)
 		if name == nil
 			return
@@ -28,16 +31,22 @@ class Packet
 		@data[name]
 	end
 	
+	# 패킷에 데이터를 넣는다
 	def []=(name,data)
 		push(name, data)
 	end
+	
+	# 패킷에서 데이터를 가져온다
 	def [](name)
 		get(name)
 	end
 
+	# 패킷을 전송할 채널을 설정한다
 	def ch=(ch)
 		@ch = ch
 	end
+	
+	# 패킷의 채널 값을 가져온다
 	def ch
 		@ch
 	end
