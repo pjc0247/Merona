dc8f1e642f6f83f4a626ff9ddf9e7ed54906129e
Anz_MSI <pjc0247@naver.com>
2013-02-27 19:23:05 +0900

    add comment

--- Merona/lib/server/connection.rb
+++ Merona/lib/server/connection.rb
@@ -6,6 +6,7 @@ class Connection < EM::Connection
 	attr_accessor :id
 	attr_reader :ip, :port
 	
+	# 연결을 초기화할 때 불려지는 메소드
 	def initialize(*args)
 		@port, @ip = Socket.unpack_sockaddr_in(get_peername)
 		@server = args[0]
@@ -18,6 +19,7 @@ class Connection < EM::Connection
 		
 		set_sock_opt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, CONNECTION_NODELAY)
 	end
+	# 연결을 정리할 때 불려지는 메소드
 	def unbind
 		@server.clients.delete self
 		@db.dispose
@@ -26,17 +28,27 @@ class Connection < EM::Connection
 		disconnect
 	end
 	
+	# 현재 클라이언트에 패킷을 전송한다
+	#
+	# packet : 보낼 패킷
 	def send(packet)
 		send_object packet
 	end
+	
+	# 현재 클라이언트가 패킷을 받았을 때 불려지는 메소드
+	#
+	# obj : 수신된 패킷
 	def receive_object(obj)
 		p obj
 		@server.process.enqueue(self, obj)
 	end
 	
+	
+	# 연결이 수립되었을 때 불려지는 메소드
 	def connect
 		Log.output("new connection from " + @ip)
 	end
+	# 연결이 종료될 때 불려지는 메소드
 	def disconnect
 		Log.output("lost connection " + @ip)
 	end
