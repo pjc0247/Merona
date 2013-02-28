7b1517242e35c48bfea89e6c37102e2318920a50
Anz_MSI <pjc0247@naver.com>
2013-02-28 09:38:43 +0900

    add comment

--- Merona/lib/handler/handler.rb
+++ Merona/lib/handler/handler.rb
@@ -4,6 +4,11 @@ class Handler
 	def dispose
 	end
 	
+	# 패킷을 수신했을 때 불려지는 메소드
+	#
+	# server : 수신한 서버
+	# connection : 패킷을 보낸 클라이언트
+	# packet : 수신한 패킷
 	def recv(server,connection,packet)
 		#case packet.id
 		#	when Protocol::PacketID
